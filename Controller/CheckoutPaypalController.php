268430b6d4797cc32f9406a4b352b35a24060291
Jonas Renaudot <jonas.renaudot@gmail.com>
2013-02-06 19:30:06 +0100

    Fix risk of paypal IPN bad validation

--- Controller/CheckoutPaypalController.php
+++ Controller/CheckoutPaypalController.php
@@ -45,31 +45,34 @@ class CheckoutPaypalController extends Controller{
 		
 		//DO IPN STUFF - VALIDATE OR UNVALIDATE WITH ORDER MANAGER
 		
-		
-		foreach($ipnMessage->getRawData() as $key => $value) {
-			$logger->err("DATA : " . $key . " = " . $value);
-		}
-		
-		if($validate) {
-			$logger->debug("Success: Got valid IPN data");
+		try{
+			foreach($ipnMessage->getRawData() as $key => $value) {
+				$logger->err("DATA : " . $key . " = " . $value);
+			}
 			
-			$command = $orderManager->getCommand($command);
-			
-			$data = $paypalManager->getExpressCheckoutDetails($command->getToken());
-			$checkoutStatus = $data->GetExpressCheckoutDetailsResponseDetails->CheckoutStatus;
-			
-	    	if($checkoutStatus == 'PaymentActionCompleted'){
-   				$orderManager->validateCommand($command);
-		   	}else if($checkoutStatus == 'PaymentActionInProgress'){
-		   		//do nothing
-		   	}else if($checkoutStatus == 'PaymentActionFailed'){
-		   		$orderManager->errorCommand($command);
-		   		$logger->err("PAYPAL IPN command " . $command->getId() . " PaymentActionFailed");
-	    	}
-			
-		} else {
-			$logger->err("Error: Got invalid IPN data");
-			return new Response("Invalid IPN data");
+			if($validate) {
+				$logger->debug("Success: Got valid IPN data");
+				
+				$command = $orderManager->getCommand($command);
+				
+				$data = $paypalManager->getExpressCheckoutDetails($command->getToken());
+				$checkoutStatus = $data->GetExpressCheckoutDetailsResponseDetails->CheckoutStatus;
+				
+		    	if($checkoutStatus == 'PaymentActionCompleted'){
+	   				$orderManager->validateCommand($command);
+			   	}else if($checkoutStatus == 'PaymentActionInProgress'){
+			   		//do nothing
+			   	}else if($checkoutStatus == 'PaymentActionFailed'){
+			   		$orderManager->errorCommand($command);
+			   		$logger->err("PAYPAL IPN command " . $command->getId() . " PaymentActionFailed");
+		    	}
+				
+			} else {
+				$logger->err("Error: Got invalid IPN data");
+				return new Response("Invalid IPN data");
+			}
+		}catch(\Exception $e){
+			$logger->err("PAYPAL IPN FATAL ERROR : " . $e->getMessage());
 		}
 		
 		return new Response("Finished correctly");
